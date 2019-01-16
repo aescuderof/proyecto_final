0256f9467069ccecd2c85616d11ec78085b778d5
aescuderof <antoescudero@gmail.com>
2019-01-16 20:06:46 -0300

    galeria de fotos

--- app.js
+++ app.js
@@ -28,3 +28,14 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+
+
+
+
+jQuery('.photo-thumbnails .thumbnail').click(function() {
+        jQuery('.photo-thumbnails .thumbnail').removeClass('current');
+        jQuery(this).addClass('current');
+        var path = jQuery(this).find('img').attr('src');
+        jQuery('#big-photo img').attr('src', path);
+    });
